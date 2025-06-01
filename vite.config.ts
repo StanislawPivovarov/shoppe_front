02a3b0f48065eef6a303514424d54d5554d7c696
Стас Пивоваров <stas.povovarov@headmade.pro>
2025-06-01 21:13:42 +0300

    fix: return base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/shoppe_front/',
+  base: '/shoppe_front/',
   plugins: [react()],
 })
